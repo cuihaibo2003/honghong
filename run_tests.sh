fefb7929f405971a48ee9e36fdf314cfc0ce99c3
Amelie <haibo.cui@snapsendsolve.com>
2025-06-18 04:19:44 +0800

    Add support for running a single test file

--- run_tests.sh
+++ run_tests.sh
@@ -7,20 +7,38 @@ clean_allure_cache() {
 }
 
 export PYTHONPATH=$(pwd)
-if [ "$1" == "admin" ]; then
-    echo "Running pytest for admin portal"
-    clean_allure_cache "admin_portal/testresult/allure-results"
-    pytest admin_portal/tests --alluredir=admin_portal/testresult/allure-results --maxfail=1 --disable-warnings -q
-    allure serve admin_portal/testresult/allure-results
-elif [ "$1" == "snapper" ]; then
-    echo "Running pytest for snapper portal"
-    clean_allure_cache "snapper_portal/testresult/allure-results"
-    pytest snapper_portal/tests --alluredir=snapper_portal/testresult/allure-results --maxfail=1 --disable-warnings -q
-    allure serve snapper_portal/testresult/allure-results
-else
-    echo "Please specify 'admin' or 'snapper' to run the respective tests."
+
+
+PORTAL=$1
+TEST_FILE_NAME=$2
+
+# check if the first argument is admin or snapper
+if [ "$PORTAL" != "admin" ] && [ "$PORTAL" != "snapper" ]; then
+    echo "First argument must be 'admin' or 'snapper'"
+    exit 1
 fi
+
+# Clear cache
+clean_allure_cache "${PORTAL}_portal/testresult/allure-results"
+
+if [ -z "$TEST_FILE_NAME" ]; then
+    # if no test file is specified then ran all test
+    echo "Running all tests in ${PORTAL}_portal/tests"
+    pytest ${PORTAL}_portal/tests --alluredir=${PORTAL}_portal/testresult/allure-results --maxfail=1 --disable-warnings -q
+else
+    # if a test file is specified then ran the specific test file
+    TEST_PATH="${PORTAL}_portal/tests/$TEST_FILE_NAME"
+    if [ ! -f "$TEST_PATH" ]; then
+        echo "Test file '$TEST_PATH' not found."
+        exit 1
+    fi
+    echo "Running test file $TEST_PATH"
+    pytest "$TEST_PATH" --alluredir=${PORTAL}_portal/testresult/allure-results --maxfail=1 --disable-warnings -q
+fi
+
+allure serve ${PORTAL}_portal/testresult/allure-results
 
 # Record: playwright codegen --target python https://me-dev-1.snapsendsolve.com
 # run: ./run_tests.sh snapper
 # run: ./run_tests.sh admin
+# run: ./run_tests.sh snapper file_name.py
